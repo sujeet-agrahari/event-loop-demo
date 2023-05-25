04a225d668be5e41fdbffc8478d0eb675fe180ed
Sujeet Agrahari <sujeet.rgcsm@gmail.com>
2023-05-25 07:32:42 +0530

    add more examples

--- phases-examples/example-8.js
+++ phases-examples/example-8.js
@@ -1,5 +1,11 @@
-// takeaway 2 -> process.netTick runs immediately and setImmediate runs on next tick => blame node.js maintainers 🥲
+// takeaway 1 -> process.netTick runs immediately and setImmediate runs on next tick => blame node.js maintainers 🥲
 // they accepted the mistake, but they can't change it as thousands of npm libraries depend on this, and new are being added
+
+// takeaway 2 -> If you put things in process.nextTick a lot, other callback will not be executed and will block the loop
 setImmediate(() => console.log('setImmediate'))
-process.nextTick(() => console.log('process.nextTick'))
+process.nextTick(() => console.log('process.nextTick1'))
+process.nextTick(() => console.log('process.nextTick2'))
+process.nextTick(() => console.log('process.nextTick3'))
+process.nextTick(() => console.log('process.nextTick4'))
+
 console.log('I am first..I am first...!!')
